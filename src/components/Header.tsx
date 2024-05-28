f87cc21bfd42b0b90957a51bd69d3dcec8c4268a
yusuken1121 <yusuken1121@gmail.com>
2024-05-27 17:26:39 -0700

    Change the dark mode design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header: FC<HeaderProps> = ({ onToggleDarkMode, currentImage }) => {
   return (
     <div className="flex justify-center py-11 ">
       <h2 className="text-3xl font-bold">Todo List in TypeScript</h2>
-      <div className="w-8 h-8 flex">
+      <div className="w-8 h-8 flex mx-5 border-solid border-2 border-r-8 dark:border-l-8 dark:border-r-2 hover:cursor-pointer hover:scale-110  hover:bg-lime-300 dark:hover:bg-purple-400 transition ease-in-out duration-300">
         <img
           onClick={() => {
             onToggleDarkMode();
